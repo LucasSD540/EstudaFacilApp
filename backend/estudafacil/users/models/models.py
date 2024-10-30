62ad34cc4f3fbdbbf6cbbcb88e8698bd40dcfeca
Lucas Souza Duarte <lucas.souzaduarte.73@gmail.com>
2024-10-30 11:51:15 -0300

    feat: add study_level choice to model and serializer

--- backend/estudafacil/users/models/models.py
+++ backend/estudafacil/users/models/models.py
@@ -2,12 +2,23 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
 class User(AbstractBaseUser):
+  STUDY_LEVEL_CHOICES = [
+        ('enem', 'Enem e Vestibular'),
+        ('superior', 'Ensino Superior'),
+        ('concurso', 'Concurso'),
+    ]
+
   fullName = models.CharField(max_length=200)
   email = models.EmailField(max_length=254)
   password = models.CharField(max_length=128)
   profilePicture = models.ImageField(upload_to='', blank=True, null=True)
   created_at = models.DateTimeField(auto_now_add=True)
   last_login = models.DateTimeField(blank=True, null=True)
+  study_level = models.CharField(
+        max_length=10,
+        choices=STUDY_LEVEL_CHOICES,
+        default="enem"
+    )
 
   USERNAME_FIELD = 'email'
 
